05f3bd6f6f8ea3e5632a76b4a152d0649f133b1c
vuongicesmile <boide03@gmail.com>
2022-03-12 11:48:25 +0700

    4th

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const path = require("path");
+
+module.exports = {
+  outputDir: path.resolve(__dirname, "../docs"),
+}
